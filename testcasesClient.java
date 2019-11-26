45a1955d59261e52a16e940485c84ed1738ef01e
uniqueuser-repo <57453461+uniqueuser-repo@users.noreply.github.com>
2019-11-26 03:33:53 -0600

    Add important comments

--- testcasesClient.java
+++ testcasesClient.java
@@ -1,5 +1,15 @@
+/**
+ *
+ * The client of the test cases for CS251 Project 5. Allows students to create their own tree and step through
+ * the instance with a debugger.
+ *
+ * @author Andrew Orlowski, orlowska@purdue.edu
+ * @version 11/26/2019
+ *
+ */
+
 public class testcasesClient {
     public static void main(String[] args) {
-
+      //TODO: Everything.
     }
 }
--- testcasesServer.java
+++ testcasesServer.java
@@ -2,6 +2,25 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ *  The server for the test cases of CS251 Fall'19 Project 5. It accepts input from clients (students)
+ *  as to what words should be inputted in the ternary trie. After the students are content with their
+ *  added words, the server will send the complete Object to the student. From there, they can step
+ *  through the solution trie with a debugger to see exactly how the solution trie should look.
+ *
+ *  This is particularly helpful for when students are failing a test case, but they don't
+ *  know exactly how their version differs. It allows them to walk through the trie and spot
+ *  the differences between their trie and the solution trie.
+ *
+ *  This is important because I am obviously not allowed to share code. Generally, in test cases
+ *  for academic assignments like these, I would have an instance of the solution trie available to me
+ *  freely. When creating test cases here, I can't have that, because I can't attach the solution code
+ *  along with the test cases. Doing this allows an instance to be passed without revealing the source code.
+ *
+ * @author Andrew Orlowski, orlowska@purdue.edu
+ * @version 11/26/2019
+ *
+ */
 public class testcasesServer {
     public static void main(String[] args) {
         try {
@@ -28,10 +47,21 @@ class ClientHandler extends Thread {
 
     public void run() {
         WordProcessor localWP = new WordProcessor();
-        InputStreamReader isr = new InputStreamReader(client.getInputStream());
-        OutputStreamWriter osw = new OutputStreamWriter(client.getOutputStream());
-        while () {
+        try {
+            InputStreamReader isr = new InputStreamReader(client.getInputStream());
+            ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream());
+            BufferedReader bfr = new BufferedReader(isr);
+            while (true) {
+                String readLine = bfr.readLine();
+                if (readLine == "exit") {
+                    //TODO: Send completed localWP object to client
+                    break;
+                }
+                //TODO: Read in single String from user, add to localWP
 
+            }
+        } catch (IOException ioe) {
+            System.out.println("IOException when creating streams!");
         }
     }
 }
