98ffc3b5433ce419fb47ed32db9f8feab8cdb9f5
uniqueuser-repo <57453461+uniqueuser-repo@users.noreply.github.com>
2019-11-26 03:41:20 -0600

    Create headlessClient(..). Will be used to call from within test cases.

--- testcasesClient.java
+++ testcasesClient.java
@@ -10,6 +10,14 @@
 
 public class testcasesClient {
     public static void main(String[] args) {
-      //TODO: Everything.
+        //TODO: Everything.
+    }
+
+    // A headless version of the test cases client that doesn't specifically
+    // need to be executed by the user. This will be used from within the test
+    // cases (specifically Test_D_AddWord_Expert) to help you narrow down
+    // precisely where you first fail the test case.
+    public static void headlessClient(String[] wordsToAdd) {
+        //TODO: Everything.
     }
 }
